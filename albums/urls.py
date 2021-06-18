286b8e44f705dc85a67410094fb1cc23e6cc4898
sarahartsoe <sarahartsoe@gmail.com>
2021-06-18 16:35:09 -0400

    updated views

--- albums/urls.py
+++ albums/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_album, name='list_album'),
-    path('/albums/new', views.new_album, name='new_album'),
-    path('/albums/<int:pk>/edit', views.edit_album, name='edit_album'),
-    path('/albums/<int:pk>/delete', views.delete_album, name='delete_album'),
+    path('', views.album_list, name='album_list'),
 ]
--- albums/views.py
+++ albums/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Album
 
 # Create your views here.
 def album_list(request):
+    albums = Album.objects.all()
     return render(request, 'albums/album_list.html', {})
