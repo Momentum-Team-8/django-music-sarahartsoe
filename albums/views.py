19a9a7f5e35694c61949b9e9abf205315e2b2d8a
sarahartsoe <sarahartsoe@gmail.com>
2021-06-18 16:58:53 -0400

    started adding css

--- albums/views.py
+++ albums/views.py
@@ -4,4 +4,4 @@ from .models import Album
 # Create your views here.
 def album_list(request):
     albums = Album.objects.all()
-    return render(request, 'albums/album_list.html', {})
+    return render(request, 'albums/album_list.html', {'albums': albums})
