f1bc77d05b9b61786ecf325994f8f14ff83ee613
sarahartsoe <sarahartsoe@gmail.com>
2021-06-20 14:57:48 -0400

    added working option to delete an albumgit add .git add .

--- albums/urls.py
+++ albums/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('albums/<int:pk>/', views.album_detail, name='album_detail'),
     path('album/new/', views.album_new, name='album_new'),
     path('albums/<int:pk>/edit/', views.album_edit, name='album_edit'),
+    path('albums/<int:pk>/delete', views.album_delete, name='album_delete'),
 ]
